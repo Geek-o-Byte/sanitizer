af48f02d03a3f7bb384643272a64799375091ef6
mixel <mimaslov@itmo.ru>
2023-06-07 23:19:16 +0300

    sd

--- sanitizer.cpp
+++ sanitizer.cpp
@@ -1,79 +1,61 @@
 #include "sanitizer.h"
 #include <dlfcn.h>
 #include <pthread.h>
+#include <unordered_map>
+#include <vector>
 
-//std::unordered_map<pthread_mutex_t*, pthread_t> mutex_lock_map;
-//std::vector<pthread_mutex_t*> mutex_lock_stack;
-//bool deadlock_found = false;
+std::unordered_map<pthread_mutex_t*, pthread_t> mutex_lock_map;
+std::vector<pthread_mutex_t*> mutex_lock_stack;
+bool deadlock_found = false;
 
 static int (*orig_pthread_mutex_lock)(pthread_mutex_t *mutex) = (int (*)(pthread_mutex_t *))dlsym(RTLD_NEXT, "pthread_mutex_lock");
 static int (*orig_pthread_mutex_unlock)(pthread_mutex_t *mutex) = (int (*)(pthread_mutex_t *))dlsym(RTLD_NEXT, "pthread_mutex_unlock");
 
+std::unordered_map<pthread_mutex_t*, std::vector<pthread_mutex_t*>> mutex_graph;
+
 extern "C" {
-    int pthread_mutex_lock(pthread_mutex_t* mutex) {
-        // Сохранение информации о захвате мьютекса
-        mutex_lock_map[mutex] = pthread_self();
-        mutex_lock_stack.push_back(mutex);
+int pthread_mutex_lock(pthread_mutex_t* mutex) {
+    mutex_lock_map[mutex] = pthread_self();
+    mutex_lock_stack.push_back(mutex);
 
-        // Вызов реальной функции pthread_mutex_lock
-        int result = orig_pthread_mutex_lock(mutex);
+    check_deadlock();
 
-        // Проверка наличия deadlock после захвата мьютекса
-        check_deadlock();
-
-        // Возврат результата выполнения реальной функции pthread_mutex_lock
-        return result;
-    }
-
-    // Обертка для функции pthread_mutex_unlock
-    int pthread_mutex_unlock(pthread_mutex_t* mutex) {
-        // Удаление информации о захвате мьютекса
-        mutex_lock_map.erase(mutex);
-        mutex_lock_stack.pop_back();
-
-        // Вызов реальной функции pthread_mutex_unlock
-        int result = orig_pthread_mutex_unlock(mutex);
-
-        // Проверка наличия deadlock после освобождения мьютекса
-        check_deadlock();
-
-        // Возврат результата выполнения реальной функции pthread_mutex_unlock
-        return result;
-    }
+    return orig_pthread_mutex_lock(mutex);
 }
 
-// Функция для проверки deadlock с помощью DFS
+int pthread_mutex_unlock(pthread_mutex_t* mutex) {
+    mutex_lock_map.erase(mutex);
+    mutex_lock_stack.pop_back();
+
+    check_deadlock();
+
+    return orig_pthread_mutex_unlock(mutex);
+}
+}
+
 void check_deadlock_helper(pthread_mutex_t* mutex, std::unordered_map<pthread_mutex_t*, int>& color) {
-    color[mutex] = 1; // Красим текущий мьютекс в серый
+    color[mutex] = 1;
 
-    for (auto it = mutex_lock_map.begin(); it != mutex_lock_map.end(); ++it) {
-        pthread_mutex_t* next_mutex = it->first;
-        pthread_t thread_id = it->second;
-
-        if (next_mutex == mutex) {
-            continue; // Пропустить текущий мьютекс
-        }
-
+    for (pthread_mutex_t* next_mutex : mutex_graph[mutex]) {
         if (color[next_mutex] == 0) {
-            check_deadlock_helper(next_mutex, color); // Рекурсивный вызов для следующего мьютекса
+            check_deadlock_helper(next_mutex, color);
 
             if (deadlock_found) {
-                return; // Если deadlock уже найден, выходим из функции
+                return;
             }
         }
-        else if (color[next_mutex] == 1 && thread_id == pthread_self()) {
-            deadlock_found = true; // Найден потенциальный deadlock
+        else if (color[next_mutex] == 1 && mutex_lock_map[next_mutex] == pthread_self()) {
+            deadlock_found = true;
             return;
         }
     }
 
-    color[mutex] = 2; // Красим текущий мьютекс в черный
+    color[mutex] = 2;
 }
 
-// Функция для проверки наличия deadlock
 void check_deadlock() {
     if (deadlock_found) {
-        return; // Если deadlock уже найден, прекращаем проверку
+        return;
     }
 
     std::unordered_map<pthread_mutex_t*, int> color;
@@ -82,7 +64,7 @@ void check_deadlock() {
         pthread_mutex_t* mutex = it->first;
 
         if (color[mutex] == 0) {
-            check_deadlock_helper(mutex, color); // Запуск DFS для каждого мьютекса
+            check_deadlock_helper(mutex, color);
         }
     }
 }
