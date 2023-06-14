05a9499f654d9b516aa5afcb826f7de5d75eeccd
mixel <mimaslov@itmo.ru>
2023-06-15 02:08:52 +0300

    try

--- sanitizer.cpp
+++ sanitizer.cpp
@@ -6,74 +6,74 @@ MutexGraph mutexGraph;
 DeadlockData deadlockData;
 
 static int (*orig_pthread_mutex_lock)(pthread_mutex_t *mutex) =
-    (int (*)(pthread_mutex_t *))dlsym(RTLD_NEXT, "pthread_mutex_lock");
+(int (*)(pthread_mutex_t *))dlsym(RTLD_NEXT, "pthread_mutex_lock");
 static int (*orig_pthread_mutex_unlock)(pthread_mutex_t *mutex) =
-    (int (*)(pthread_mutex_t *))dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+(int (*)(pthread_mutex_t *))dlsym(RTLD_NEXT, "pthread_mutex_unlock");
 
 extern "C" {
 int pthread_mutex_lock(pthread_mutex_t *mutex) {
-  deadlockData.lockMap[mutex] = pthread_self();
-  deadlockData.lockStack.push_back(mutex);
+    deadlockData.lockMap[mutex] = pthread_self();
+    deadlockData.lockStack.push_back(mutex);
 
-  // Добавление мьютекса в граф и создание ребер от предыдущих мьютексов
-  if (!deadlockData.lockStack.empty()) {
-    pthread_mutex_t *prevMutex = deadlockData.lockStack.back();
-    mutexGraph.graph[prevMutex].push_back(mutex);
-  }
+    // Добавление мьютекса в граф и создание ребер от предыдущих мьютексов
+    if (!deadlockData.lockStack.empty()) {
+        pthread_mutex_t *prevMutex = deadlockData.lockStack.back();
+        mutexGraph.graph[prevMutex].push_back(mutex);
+    }
 
-  checkDeadlock(mutexGraph, deadlockData);
+    checkDeadlock(mutexGraph, deadlockData);
 
-  return orig_pthread_mutex_lock(mutex);
+    return orig_pthread_mutex_lock(mutex);
 }
 
 int pthread_mutex_unlock(pthread_mutex_t *mutex) {
-  deadlockData.lockMap.erase(mutex);
-  deadlockData.lockStack.pop_back();
+    deadlockData.lockMap.erase(mutex);
+    deadlockData.lockStack.pop_back();
 
-  // Удаление мьютекса из графа
-  mutexGraph.graph.erase(mutex);
+    // Удаление мьютекса из графа
+    mutexGraph.graph.erase(mutex);
 
-  checkDeadlock(mutexGraph, deadlockData);
+    checkDeadlock(mutexGraph, deadlockData);
 
-  return orig_pthread_mutex_unlock(mutex);
+    return orig_pthread_mutex_unlock(mutex);
 }
 }
 
 void checkDeadlockHelper(pthread_mutex_t *mutex,
                          std::unordered_map<pthread_mutex_t *, int> &color,
                          MutexGraph &mutexGraph, DeadlockData &deadlockData) {
-  color[mutex] = 1;
+    color[mutex] = 1;
 
-  for (pthread_mutex_t *nextMutex : mutexGraph.graph[mutex]) {
-    if (color[nextMutex] == 0) {
-      checkDeadlockHelper(nextMutex, color, mutexGraph, deadlockData);
+    for (pthread_mutex_t *nextMutex : mutexGraph.graph[mutex]) {
+        if (color[nextMutex] == 0) {
+            checkDeadlockHelper(nextMutex, color, mutexGraph, deadlockData);
 
-      if (deadlockData.deadlockFound) {
-        return;
-      }
-    } else if (color[nextMutex] == 1 &&
-               deadlockData.lockMap[nextMutex] == pthread_self()) {
-      deadlockData.deadlockFound = true;
-      return;
+            if (deadlockData.deadlockFound) {
+                return;
+            }
+        } else if (color[nextMutex] == 1 &&
+                   deadlockData.lockMap[nextMutex] == pthread_self()) {
+            deadlockData.deadlockFound = true;
+            return;
+        }
     }
-  }
 
-  color[mutex] = 2;
+    color[mutex] = 2;
 }
 
 void checkDeadlock(MutexGraph &mutexGraph, DeadlockData &deadlockData) {
-  if (deadlockData.deadlockFound) {
-    return;
-  }
-
-  std::unordered_map<pthread_mutex_t *, int> color;
-
-  for (auto it = deadlockData.lockMap.begin(); it != deadlockData.lockMap.end();
-       ++it) {
-    pthread_mutex_t *mutex = it->first;
-
-    if (color[mutex] == 0) {
-      checkDeadlockHelper(mutex, color, mutexGraph, deadlockData);
+    if (deadlockData.deadlockFound) {
+        return;
     }
-  }
+
+    std::unordered_map<pthread_mutex_t *, int> color;
+
+    for (auto it = deadlockData.lockMap.begin(); it != deadlockData.lockMap.end();
+         ++it) {
+        pthread_mutex_t *mutex = it->first;
+
+        if (color[mutex] == 0) {
+            checkDeadlockHelper(mutex, color, mutexGraph, deadlockData);
+        }
+    }
 }
